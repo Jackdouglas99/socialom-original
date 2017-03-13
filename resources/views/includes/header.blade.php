1bc29bf629761e7852e248c11e3ad2467e8f8066
Jackdouglas99 <dc00577115@student.don.ac.uk>
2017-03-13 18:04:50 +0000

    Changed the application name Made nav pull application name from the config and display it

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{route('dashboard')}}">Brand</a>
+            <a class="navbar-brand" href="{{route('dashboard')}}">{{Config::get('app.name')}}</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
